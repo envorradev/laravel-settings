afd6c4b88445a28562223138c55da0ea90c275cd
Sam Taylor <sam@taylornetwork.ca>
2022-08-23 09:18:30 -0400

    phpstorm why did you remove these?

---
+++ tests/Environment/Database/Seeders/Seeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Envorra\LaravelSettings\Tests\Environment\Database\Seeders;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder as EloquentSeeder;
+
+abstract class Seeder extends EloquentSeeder
+{
+    /**
+     * @var class-string<Model>
+     */
+    public static string $model;
+
+    abstract public static function seed(): array;
+
+    public static function seedCount(): int
+    {
+        return count(static::seed());
+    }
+
+    /**
+     * @return void
+     */
+    public function run(): void
+    {
+        foreach (static::seed() as $item) {
+            /** @phpstan-ignore-next-line */
+            static::$model::create($item);
+        }
+    }
+}
